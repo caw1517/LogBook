49de70eb1439a08e95f03f06144f9e4ba3211287
caw1517 <w_canaan@yahoo.com>
2022-12-07 03:19:34 -0600

    Push for branching

---
+++ client/src/components/logbooks/LogbookCard.js
@@ -0,0 +1,7 @@
+export default function LogbookCard() {
+    return(
+        <div className={"logbookCard"}>
+            
+        </div>
+    )
+}
--- client/src/pages/LogBook.js
+++ client/src/pages/LogBook.js
@@ -1,8 +1,10 @@
 import AircraftSideBar from "../components/sidebar/Sidebar";
+import LogbookCard from "../components/logbooks/LogbookCard";
 export default function LogBook() {
     return (
         <div>
             <AircraftSideBar />
+            <LogbookCard />
         </div>
     )
 }
